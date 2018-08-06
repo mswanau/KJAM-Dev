2e0ef2bcd545bb308d2ec8038551c1f971814ab2
Matthew Swan <mattswan@live.com>
2018-08-06 16:29:58 +1000

    Created db.json

--- AusGov/src/app/user.ts
+++ AusGov/src/app/user.ts
@@ -4,7 +4,9 @@ export class User {
         public id: number,
         public first_name: string,
         public last_name: string,
+        public address: string,
         public email: string,
+        public phone: string,
         public password: string,
         public dob: string,
     ) {}
