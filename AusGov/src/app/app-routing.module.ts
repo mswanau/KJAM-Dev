615e0eaa9d00073308f1943948111ab2ccd56dee
Matthew Swan <mattswan@live.com>
2018-08-19 11:49:08 +1000

    Created password reset component

--- AusGov/src/app/app-routing.module.ts
+++ AusGov/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
 import { ProgressPageComponent } from './progress-page/progress-page.component';
 import { CentrelinkDashboardComponent } from './centrelink-dashboard/centrelink-dashboard.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'UnderConstruction', component: ProgressPageComponent },
-  { path: 'dashboard/centrelink', component: CentrelinkDashboardComponent }
+  { path: 'dashboard/centrelink', component: CentrelinkDashboardComponent },
+  { path: 'reset', component: PasswordResetComponent }
 ];
 
 @NgModule({
