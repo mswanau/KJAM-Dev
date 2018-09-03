8115f976d1dd8e53518c3d74a6af7484869bb3dc
Matthew Swan <mattswan@live.com>
2018-09-03 18:57:33 +1000

    Updated register component

--- University/src/app/register/register.component.ts
+++ University/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-register',
@@ -11,12 +12,20 @@ export class RegisterComponent implements OnInit {
   model: any = {};
 
   constructor(
-    private router: Router
+    private router: Router,
+    private students: StudentService
   ) { }
 
   ngOnInit() {
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.students.updateStudent(this.model.studentNo, this.model.course, this.model.institute, 
+      this.model.startDate, this.model.endDate, this.model.ausGov).subscribe(
+        students => {
+          this.router.navigate(['/submitted'])
+        }
+      )
+  }
 
 }
