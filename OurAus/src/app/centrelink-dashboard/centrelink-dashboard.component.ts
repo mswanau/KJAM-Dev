23beafa77d1e468bf04a52f109772ec697608715
kurtismason <n9167986@qut.edu.au>
2018-10-19 21:02:43 +1000

    small changes

--- OurAus/src/app/centrelink-dashboard/centrelink-dashboard.component.ts
+++ OurAus/src/app/centrelink-dashboard/centrelink-dashboard.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CentrelinkDashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+    
+   }
+  
   ngOnInit() {
+    
   }
 
 }
